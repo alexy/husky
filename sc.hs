adaef6a9c51bfbe3de7ee9c2ffee5e66af9eac86
Alexy Khrabrov <deliverable@gmail.com>
2010-06-16 21:17:21 -0400

    IO Graph, IntMap as M

--- sc.hs
+++ sc.hs
@@ -1,9 +1,10 @@
 import System (getArgs)
 import System.IO
-import qualified Data.Map as M
+import qualified Data.IntMap as M
 import BinaryGraph
 import SocRun
 import Data.Maybe (listToMaybe)
+import Graph
 
 eprintln s = do
 	hPutStrLn stderr s
@@ -17,9 +18,9 @@ main = do
   	", saving dcaps in " ++ saveName)
   let maxDays :: Maybe Int 
       maxDays = listToMaybe . map read $ restArgs
-  dreps <- loadData drepsName
+  dreps <- loadData drepsName :: IO Graph
   eprintln ("loaded " ++ drepsName ++ ", " ++ (show . M.size $ dreps))
-  dments <- loadData dmentsName
+  dments <- loadData dmentsName :: IO Graph
   eprintln ("loaded " ++ dmentsName ++ ", " ++ (show . M.size $ dments))
   
   let SGraph{dcapsSG =dcaps} = socRun dreps dments optSocRun {maxDaysSR= maxDays}
