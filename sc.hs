56b6f035376d6de6ff04c2f242fff86416b14506
Alexy Khrabrov <deliverable@gmail.com>
2010-06-26 22:33:41 -0400

    intern both with the common dictionary, or user ids reset!

--- sc.hs
+++ sc.hs
@@ -17,15 +17,19 @@ eprintln s = do
 -- may yse Sustem.FilePath.takeExtension repeatedly:
 suffix = flip isSuffixOf
 
-loadAnyGraph :: String -> IO Graph
-loadAnyGraph fileName = 
-  if suffix fileName ".hsb.zip" then
-    loadData fileName
+loadAnyGraph :: String -> String -> IO (Graph, Graph)
+loadAnyGraph f1 f2 = 
+  if suffix f1 ".hsb.zip" then do
+    g1 <- loadData f1
+    g2 <- loadData f2
+    return (g1,g2)
   else 
-  if suffix fileName ".json.hdb" then do
+  if suffix f1 ".json.hdb" then do
     -- may dump dic on disk right there:
-    (dic,graph) <- runTCM (fetchGraph fileName IntBS.empty Nothing (Just 10000))
-    return graph
+    -- TODO there gotta be some monadic gymnastics for that!
+    (dic,g1) <- runTCM (fetchGraph f1 IntBS.empty Nothing (Just 10000))
+    (dic,g2) <- runTCM (fetchGraph f2 dic         Nothing (Just 10000))
+    return (g1,g2)
   else error "unrecognized graph file extension" 
 
 main :: IO ()
@@ -36,9 +40,8 @@ main = do
   	", saving dcaps in " ++ saveName)
   let maxDays :: Maybe Int 
       maxDays = listToMaybe . map read $ restArgs
-  dreps <- loadAnyGraph  drepsName
-  eprintln ("loaded " ++ drepsName ++ ", " ++ (show . M.size $ dreps))
-  dments <- loadAnyGraph dmentsName
+  (dreps, dments) <- loadAnyGraph drepsName dmentsName
+  eprintln ("loaded " ++ drepsName  ++ ", " ++ (show . M.size $ dreps))
   eprintln ("loaded " ++ dmentsName ++ ", " ++ (show . M.size $ dments))
   
   let SGraph{dcapsSG =dcaps} = socRun dreps dments optSocRun {maxDaysSR= maxDays}
